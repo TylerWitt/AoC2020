8d9e4ecea0443beb5f2eedf646344e868b47ced6
Tyler Witt <ty.mo.witt@gmail.com>
2020-12-04 21:01:05 -0600

    Add part 2 to day 1

---
+++ day_1_expense_report/part_2.exs
@@ -0,0 +1,24 @@
+"input.txt"
+|> File.read!()
+|> String.split("\n")
+|> Enum.reject(& &1 == "")
+|> Enum.map(&String.to_integer/1)
+# I'm willing to bet that because there are 3 numbers, the high end of this list won't have
+# the solution.
+|> Enum.sort()
+|> Enum.reduce_while([], fn int, acc ->
+  case Enum.find(acc, &elem(&1, 0) == int && Enum.count(elem(&1, 1)) == 2) do
+    {_target, [num_1, num_2]} ->
+      {:halt, int * num_1 * num_2}
+    _ ->
+      new_entries =
+        for {_key, [val]} <- acc, val + int < 2020 do
+          {2020 - val - int, [val, int]}
+        end
+
+      new_acc = acc ++ [{2020 - int, [int]} | new_entries]
+
+      {:cont, new_acc}
+  end
+end)
+|> IO.inspect()
